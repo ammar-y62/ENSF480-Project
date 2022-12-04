2f5e4f380d618da9b2302acecfc92558f51b9447
LeggettBen14 <101438680+BadNewsBear14@users.noreply.github.com>
2022-12-03 19:02:07 -0700

    Add files via upload

---
+++ Movie.java
@@ -0,0 +1,25 @@
+
+
+import java.util.*;
+
+
+
+class Movie{
+
+	private String title;
+
+	private Showroom showroom;
+	
+	
+	
+	
+	public String getMovieName(){
+		return this.title;
+	}
+	
+	public Showroom getShowroom(){
+		return this.showroom;
+	}
+	
+
+}
