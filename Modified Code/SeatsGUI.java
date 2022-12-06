60abc2182d86a0e451d5ecacf4be711b97990568
sankim11 <90352983+sankim11@users.noreply.github.com>
2022-12-05 20:50:49 -0700

    Add files via upload

--- Modified Code/SeatsGUI.java
+++ Modified Code/SeatsGUI.java
@@ -15,7 +15,7 @@ public class SeatsGUI extends GUI implements ActionListener
     private JLabel instructions;
     private String[] columns = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
     JButton exit = new JButton("Exit");
-    JButton selectShowRoom = new JButton("Select Show Time");
+    JButton selectSeat = new JButton("Select Seat");
     
     int row = 10, column = 10;
     JPanel headerPanel = new JPanel();
@@ -32,9 +32,11 @@ public class SeatsGUI extends GUI implements ActionListener
     Theatre theatre;
     User user;
     ShowRooms showroom;
+    Seats seat;
     Vector<Seats>seatVector;
     RegisteredUser ruser;
     TicketingSystem system;
+    String ticketID;
     
     Connection dbConnect;
     Statement stmt;
@@ -43,11 +45,11 @@ public class SeatsGUI extends GUI implements ActionListener
     public SeatsGUI(Seats seat)
     {
         gui = new GUI("Seats");
-        this.showroom = showroom;
+        this.seat = seat;
         this.seatVector = this.showroom.getSeats();
         int i = 0;
         int j = 0;
-        String [][] showTimeList = new String[this.seatVector.size()][2];
+        String [][] seatList = new String[this.seatVector.size()][2];
         while(i < this.seatVector.size())
         {
             if(j == 10)
@@ -55,9 +57,13 @@ public class SeatsGUI extends GUI implements ActionListener
                 j = 0;
                 i++;
             }
-            showTimeList[i][j] = Integer.toString(this.seatVector.get(i).getSeatAvailability());
+            seatList[i][j] = Integer.toString(this.seatVector.get(i).getSeatAvailability());
             j++;
         }
+        setupSeatsMap(seatList);
+        setSize(325,300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+        gui.setVisible(true);
     }
     
     public void setupSeatsMap(String[][] data){
@@ -65,7 +71,7 @@ public class SeatsGUI extends GUI implements ActionListener
         table = new JTable(data, columns);
         seatNumberInstructions = new JLabel("Please enter the seat ID");
         
-        selectShowRoom.addActionListener(this);
+        selectSeat.addActionListener(this);
         exit.addActionListener(this);
         
         headerPanel.setLayout(new FlowLayout());
@@ -80,7 +86,7 @@ public class SeatsGUI extends GUI implements ActionListener
         panel.add(seatNumberInstructions);
         panel.add(seatID);
         
-        endPanel.add(selectShowRoom);
+        endPanel.add(selectSeat);
         endPanel.add(exit);
         
         gui.add(headerPanel, BorderLayout.NORTH);
@@ -96,9 +102,14 @@ public class SeatsGUI extends GUI implements ActionListener
             System.exit(0);
         }
         
-        if(event.getSource() == selectShowRoom)
+        if(event.getSource() == selectSeat)
         {
-            
+            if(this.seat.getSeatAvailability() == 1) {
+                this.ticketID = seatID.getText();
+                this.seat.changeSeatAvailability(0);
+            } else {
+                // how do we do nothing?
+            }
         }
     }
     
